f892b886f2433c5aedd286ce6d2e6e6f9dbadc35
Thomas Mansencal <thomas.mansencal@gmail.com>
2024-01-19 22:11:30 +1300

    Implement support for Windows and Blueprint docset.

--- unreal_engine_docset.py
+++ unreal_engine_docset.py
@@ -10,10 +10,10 @@ docset from `Unreal Engine documentation <https://docs.unrealengine.com>`__:
 
 from __future__ import annotations
 
-import contextlib
 import html
 import logging
 import multiprocessing
+import os
 import re
 import sqlite3
 import time
@@ -43,15 +43,20 @@ __all__ = [
     "ApiInformation",
     "Entry",
     "MAPPING_API_TYPE_TO_ENTRY_TYPE",
+    "chdir",
     "join_path",
     "read_xml_file",
-    "collector_default",
-    "collector_nohref",
+    "collector_cpp_default",
+    "collector_cpp_nohref",
     "COLLECTORS_CPP",
+    "collector_blueprint_default",
+    "COLLECTORS_BLUEPRINT",
     "collect_api_name_and_syntax",
     "collect_api_information",
     "process_cpp_html_file",
     "process_cpp_docset",
+    "process_blueprint_html_file",
+    "process_blueprint_docset",
     "generate_database",
     "generate_plist",
     "generate_docset",
@@ -132,6 +137,28 @@ MAPPING_API_TYPE_TO_ENTRY_TYPE: dict = {
 """Mapping of *Unreal Engine* *API* type to *Dash* entry type."""
 
 
+class chdir:
+    """
+    Define a context manager to change the current working directory.
+
+    Parameters
+    ----------
+    path
+        Desired working directory.
+    """
+
+    def __init__(self, path: Path | str):
+        self.path = path
+        self._old_cwd = []
+
+    def __enter__(self):
+        self._old_cwd.append(os.getcwd())
+        os.chdir(self.path)
+
+    def __exit__(self, *excinfo):
+        os.chdir(self._old_cwd.pop())
+
+
 def join_path(parent: Path | str, name: str) -> str:
     """
     Join given parent and name into a path.
@@ -149,7 +176,9 @@ def join_path(parent: Path | str, name: str) -> str:
         Joined path.
     """
 
-    return f'{str(parent).replace("/index.html", "")}/{name}'
+    parent = re.sub(r"\\?/?index.html$", "", str(parent))
+
+    return f"{parent}/{name}"
 
 
 def read_xml_file(xml_path: Path | str, attempts: int = 10) -> lxml.html.HtmlElement:
@@ -190,13 +219,13 @@ def read_xml_file(xml_path: Path | str, attempts: int = 10) -> lxml.html.HtmlEle
     raise RuntimeError('Could not parse "%s" file!', xml_path)
 
 
-def collector_default(
+def collector_cpp_default(
     elements: List[lxml.html.HtmlElement],
-    api_information: ApiInformation,
+    parent_api_information: ApiInformation,
     html_path: Path,
 ) -> List[Tuple[str, str]]:
     """
-    Collect the *API* data for given elements.
+    Collect the *C++* *API* data for given elements.
 
     This is the default collector.
 
@@ -204,8 +233,9 @@ def collector_default(
     ----------
     elements
         Elements to collect the data from.
-    api_information
-        *Unreal Engine* object and *Dash* documentation *API* information.
+    parent_api_information
+        Parent *Unreal Engine* object and *Dash* documentation *API*
+        information.
     html_path
         Path of the file the elements are contained into.
 
@@ -229,20 +259,21 @@ def collector_default(
     return collection
 
 
-def collector_nohref(
+def collector_cpp_nohref(
     elements: List[lxml.html.HtmlElement],
-    api_information: ApiInformation,
+    parent_api_information: ApiInformation,
     html_path: Path,
 ) -> List[Tuple[str, str]]:
     """
-    Collect the *API* data for given elements without an ``href``.
+    Collect the *C++* *API* data for given elements without an ``href``.
 
     Parameters
     ----------
     elements
         Elements to collect the data from.
-    api_information
-        *Unreal Engine* object and *Dash* documentation *API* information.
+    parent_api_information
+        Parent *Unreal Engine* object and *Dash* documentation *API*
+        information.
     html_path
         Path of the file the elements are contained into.
 
@@ -255,8 +286,8 @@ def collector_nohref(
     collection = []
     for element in elements:
         name = element.text_content()
-        if api_information.ue_type is not None:
-            name = f"{api_information.name}.{name}"
+        if parent_api_information.ue_type is not None:
+            name = f"{parent_api_information.name}.{name}"
 
         collection.append((name, f"{html_path}#{name}"))
 
@@ -266,71 +297,128 @@ def collector_nohref(
 COLLECTORS_CPP: Tuple[Collector, ...] = (
     Collector(
         "Module",
-        './/div[@class="modules-list"]//td[@class="name-cell"]/a',
-        collector_default,
+        './/div[@class="modules-list"]//td[@class="name-cell"]/a[not(@class="dashAnchor")]',
+        collector_cpp_default,
     ),
     Collector(
         "Class",
-        './/div[@id="classes"]//td[@class="name-cell"][1]/a',
-        collector_default,
+        './/div[@id="classes"]//td[@class="name-cell"][1]/a[not(@class="dashAnchor")]',
+        collector_cpp_default,
     ),
     Collector(
         "Constructor",
-        './/div[@id="constructor"]//td[@class="name-cell"][1]/a',
-        collector_default,
+        './/div[@id="constructor"]//td[@class="name-cell"][1]/a[not(@class="dashAnchor")]',
+        collector_cpp_default,
     ),
     Collector(
         "Destructor",
-        './/div[@id="destructor"]//td[@class="name-cell"][1]/a',
-        collector_default,
+        './/div[@id="destructor"]//td[@class="name-cell"][1]/a[not(@class="dashAnchor")]',
+        collector_cpp_default,
     ),
     Collector(
         "Type",
-        './/div[@id="typedefs"]//td[@class="name-cell"][1]/a',
-        collector_default,
+        './/div[@id="typedefs"]//td[@class="name-cell"][1]/a[not(@class="dashAnchor")]',
+        collector_cpp_default,
     ),
     Collector(
         "Enum",
-        './/div[@id="enums"]//td[@class="name-cell"][1]/a',
-        collector_default,
+        './/div[@id="enums"]//td[@class="name-cell"][1]/a[not(@class="dashAnchor")]',
+        collector_cpp_default,
     ),
     Collector(
         "Variable",
-        './/div[@id="variables"]//td[@class="name-cell"][2]/a',
-        collector_default,
+        './/div[@id="variables"]//td[@class="name-cell"][2]/a[not(@class="dashAnchor")]',
+        collector_cpp_default,
     ),
     Collector(
         "Variable",
-        './/div[@id="deprecatedvariables"]//td[@class="name-cell"][2]/a',
-        collector_default,
+        './/div[@id="deprecatedvariables"]//td[@class="name-cell"][2]/a[not(@class="dashAnchor")]',
+        collector_cpp_default,
     ),
     Collector(
         "Variable",
         './/div[@id="variables"]//td[@class="name-cell"][2]/p',
-        collector_nohref,
+        collector_cpp_nohref,
     ),
     Collector(
         "Variable",
         './/div[@id="deprecatedvariables"]//td[@class="name-cell"][2]/p',
-        collector_nohref,
+        collector_cpp_nohref,
     ),
     Collector(
         "Constant",
-        './/div[@id="constants"]//td[@class="name-cell"][1]/a',
-        collector_default,
+        './/div[@id="constants"]//td[@class="name-cell"][1]/a[not(@class="dashAnchor")]',
+        collector_cpp_default,
     ),
     Collector(
         "Function",
-        './/div[starts-with(@id, "functions_")]//td[@class="name-cell"][2]/a',
-        collector_default,
+        './/div[starts-with(@id, "functions_")]//td[@class="name-cell"][2]/a[not(@class="dashAnchor")]',
+        collector_cpp_default,
     ),
     Collector(
         "Function",
-        './/div[starts-with(@id, "deprecatedfunctions")]//td[@class="name-cell"][2]/a',
-        collector_default,
+        './/div[starts-with(@id, "deprecatedfunctions")]//td[@class="name-cell"][2]/a[not(@class="dashAnchor")]',
+        collector_cpp_default,
     ),
 )
-"""*Unreal Engine* documentation processors."""
+"""*Unreal Engine* documentation collectors for *C++*."""
+
+
+def collector_blueprint_default(
+    elements: List[lxml.html.HtmlElement],
+    parent_api_information: ApiInformation,
+    html_path: Path,
+) -> List[Tuple[str, str]]:
+    """
+    Collect the *Blueprint++* *API* data for given elements.
+
+    This is the default collector.
+
+    Parameters
+    ----------
+    elements
+        Elements to collect the data from.
+    parent_api_information
+        Parent *Unreal Engine* object and *Dash* documentation *API*
+        information.
+    html_path
+        Path of the file the elements are contained into.
+
+    Returns
+    -------
+    :class:`list`
+        Collected data.
+    """
+
+    collection = []
+    for element in elements:
+        collected_path = join_path(html_path, element.attrib["href"])
+
+        if not Path(collected_path).exists():
+            continue
+
+        collected_name = collect_api_information(read_xml_file(collected_path)).name
+
+        collection.append(
+            (f"{parent_api_information.name}.{collected_name}", collected_path)
+        )
+
+    return collection
+
+
+COLLECTORS_BLUEPRINT: Tuple[Collector, ...] = (
+    Collector(
+        "Function",
+        './/h2[@id="actions"]/following-sibling::div[@class="member-list"]//td[@class="name-cell"]/a[not(@class="dashAnchor")]',
+        collector_blueprint_default,
+    ),
+    Collector(
+        "Category",
+        './/h2[@id="categories"]/following-sibling::div[@class="member-list"]//td[@class="name-cell"]/a[not(@class="dashAnchor")]',
+        collector_blueprint_default,
+    ),
+)
+"""*Unreal Engine* documentation collectors for *Blueprint*."""
 
 
 def collect_api_name_and_syntax(
@@ -431,6 +519,8 @@ def process_cpp_html_file(
 
     api_information = collect_api_information(xml)
 
+    has_dash_anchors = len(xml.xpath('.//a[@class="dashAnchor"]')) != 0
+
     entries = []
     for collector in collectors:
         elements = xml.xpath(collector.xpath)
@@ -438,6 +528,7 @@ def process_cpp_html_file(
         for i, collection in enumerate(
             collector.processor(elements, api_information, html_path)
         ):
+            print(collection)
             collected_name, collected_html_path = collection
 
             if not Path(collected_html_path).exists():
@@ -452,7 +543,7 @@ def process_cpp_html_file(
                 collected_api_information = collect_api_information(collected_xml)
                 collected_type = collected_api_information.dash_type
 
-            if add_dash_anchors:
+            if add_dash_anchors and not has_dash_anchors:
                 anchor_element = etree.Element("a")
                 anchor_element.set("class", "dashAnchor")
                 anchor_name = collected_name.split("::")[-1]
@@ -532,6 +623,138 @@ def process_cpp_docset(api_directory: Path) -> set[Entry]:
             *process_map(
                 process_cpp_html_file,
                 html_files,
+                chunksize=16,
+            )
+        )
+    )
+
+
+def process_blueprint_html_file(
+    html_path: Path,
+    collectors: Tuple = COLLECTORS_BLUEPRINT,
+    add_dash_anchors: bool = True,
+) -> List[Entry]:
+    """
+    Process given *Unreal Engine* *Blueprint* *HTML* file using given collectors.
+
+    Parameters
+    ----------
+    html_path
+         *HTML* file path to collect the data from.
+    collectors
+        Collectors used to process the *HTML* file.
+    add_dash_anchors
+        Whether to add *Dash* anchors to generate a
+        `TOC <https://kapeli.com/docsets#tableofcontents>`__.
+
+    Returns
+    -------
+    :class:`list`
+        List of *Dash* documentation entries.
+    """
+
+    logger.info('Processing "%s" file...', html_path)
+
+    def localiser(link):
+        """Update given link to point to an actual *HTML* file"""
+
+        index_path = html_path.parent / Path(link) / "index.html"
+
+        if not index_path.exists():
+            return link
+
+        return f"{link}/index.html"
+
+    xml = read_xml_file(html_path)
+    xml.rewrite_links(localiser)
+
+    api_information = collect_api_information(xml)
+
+    has_dash_anchors = len(xml.xpath('.//a[@class="dashAnchor"]')) != 0
+
+    entries = []
+    for collector in collectors:
+        elements = xml.xpath(collector.xpath)
+        for i, collection in enumerate(
+            collector.processor(elements, api_information, html_path)
+        ):
+            collected_name, collected_html_path = collection
+
+            if not Path(collected_html_path).exists():
+                continue
+
+            collected_type = collector.type
+
+            if add_dash_anchors and not has_dash_anchors:
+                anchor_element = etree.Element("a")
+                anchor_element.set("class", "dashAnchor")
+
+                anchor_element.set(
+                    "name",
+                    f"//apple_ref/blueprint/{collected_type}/{html.escape(collected_name)}",
+                )
+                elements[i].addprevious(anchor_element)
+
+            entries.append(
+                Entry(
+                    cast(str, collected_name),
+                    cast(str, collected_html_path),
+                    cast(str, collected_type),
+                )
+            )
+
+    with open(html_path, "w") as html_file:
+        html_file.write(tostring(xml).decode("utf-8"))  # pyright: ignore
+
+    return entries
+
+
+def process_blueprint_docset(api_directory: Path) -> set[Entry]:
+    """
+    Process given *Unreal Engine* *Blueprint* docset.
+
+    Parameters
+    ----------
+    api_directory
+         Docset *API* path, e.g.``en-US/BlueprintAPI``.
+
+    Returns
+    -------
+    :class:`set`
+        Set of entries.
+    """
+
+    logger.info("Processing Blueprint docset...")
+
+    css_path = api_directory / ".." / ".." / "Include" / "CSS" / "udn_public.css"
+    with open(css_path, "a") as css_file:
+        css_file.write(
+            """
+#maincol {
+    height: unset !important;
+}
+
+#page_head, #navWrapper, #splitter, #footer {
+    display: none !important;
+}
+
+#contentContainer {
+    margin-left: 0 !important;
+}
+
+.toc {
+    display: none !important;
+}
+"""
+        )
+
+    html_files = list(api_directory.glob("**/*.html"))
+
+    return set(
+        chain(
+            *process_map(
+                process_blueprint_html_file,
+                html_files,
                 max_workers=multiprocessing.cpu_count() - 2,
                 chunksize=16,
             )
@@ -570,13 +793,15 @@ def generate_database(
     except sqlite3.OperationalError as error:
         logging.warning(str(error))
 
+    documents_directory = f"{documents_directory}/".replace("\\", "/")
+
     for entry in sorted(entries, key=lambda x: x.name):
         cursor.execute(
             "INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)",
             (
                 entry.name,
                 entry.type,
-                entry.path.replace(f"{documents_directory}/", ""),
+                entry.path.replace("\\", "/").replace(documents_directory, ""),
             ),
         )
 
@@ -669,9 +894,9 @@ def generate_docset(input: str, output: str) -> None:
     documents_directory.mkdir(parents=True, exist_ok=True)
 
     if docset_type == "blueprint":
-        api_directory = Path("None")
+        api_directory = documents_directory / "en-US" / "BlueprintAPI"
         label = "Blueprint"
-        processor = lambda x: set()  # noqa: E731
+        processor = process_blueprint_docset
         online = "https://docs.unrealengine.com/en-US/BlueprintAPI"
     elif docset_type == "cpp":
         api_directory = documents_directory / "en-US" / "API"
@@ -687,7 +912,7 @@ def generate_docset(input: str, output: str) -> None:
     logger.info('Extracting "%s" archive to "%s"...', input, documents_directory)
     setuptools.archive_util.unpack_archive(input, str(documents_directory))
 
-    with contextlib.chdir(api_directory):  # pyright: ignore
+    with chdir(api_directory):  # pyright: ignore
         entries = processor(api_directory)
 
     generate_database(
@@ -707,6 +932,8 @@ def generate_docset(input: str, output: str) -> None:
 
     if docset_type == "cpp":
         mapping.append(("dashIndexFilePath", "string", "en-US/API/index.html"))
+    elif docset_type == "cpp":
+        mapping.append(("dashIndexFilePath", "string", "en-US/BluepringAPI/index.html"))
 
     generate_plist(contents_directory / "Info.plist", mapping)
 
